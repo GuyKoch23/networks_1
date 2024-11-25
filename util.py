8caf879a723cbc2e162ca3c8b77df09aebe41807
GuyKoch <guykoch@mail.tau.ac.il>
2024-11-25 05:29:26 +0200

    utils comments

--- util.py
+++ util.py
@@ -2,44 +2,42 @@ import struct
 
 
 def sendall(sock, data):
-    """Send all data to the socket reliably."""
+    """Sends data to socket, ensures reliability"""
     total_sent = 0
     while total_sent < len(data):
-        sent = sock.send(data[total_sent:])
-        if sent == 0:
-            raise RuntimeError("Socket connection broken")
-        total_sent += sent
+        cur_sent = sock.send(data[total_sent:])
+        if cur_sent == 0:
+            raise RuntimeError("Socket connection broken...")
+        total_sent += cur_sent
 
 
-def send_message(sock, message):
-    """Send a message with a 4-byte length prefix."""
-    encoded_message = message.encode()
-    message_length = struct.pack("!I", len(encoded_message))  # 4-byte unsigned int
-    sendall(sock, message_length + encoded_message)
+def send_message(sock, msg):
+    """Sends message to socket, packing with 4-bytes length indicator number"""
+    encoded_msg = msg.encode()
+    msg_length = struct.pack("!I", len(encoded_msg))
+    sendall(sock, msg_length + encoded_msg)
 
 
-def recvall(sock, num_bytes):
-    """Receive an exact number of bytes."""
+def recvall(sock, bytes_amount):
+    """Receives all bytes_amount reliably from socket"""
     data = b""
-    while len(data) < num_bytes:
-        chunk = sock.recv(num_bytes - len(data))
-        if chunk == b"":
-            raise RuntimeError("Socket connection broken")
-        data += chunk
+    while len(data) < bytes_amount:
+        cur_chunk = sock.recv(bytes_amount - len(data))
+        if cur_chunk == b"":
+            raise RuntimeError("Socket connection broken...")
+        data += cur_chunk
     return data
 
 
 def recv_message(sock):
-    """Receive a message with a 4-byte length prefix."""
-    # Read the 4-byte length prefix
-    message_length_bytes = recvall(sock, 4)
-    message_length = struct.unpack("!I", message_length_bytes)[0]
-    # Read the actual message
-    return recvall(sock, message_length).decode()
+    """Receives message from socket, unpacking 4-bytes length indicator number"""
+    msg_length_bytes = recvall(sock, 4)
+    msg_length = struct.unpack("!I", msg_length_bytes)[0]
+    return recvall(sock, msg_length).decode()
 
 
 def handle_input(sock):
-    """Handle user input."""
+    """Handles user input"""
     input_text = input("")
     if input_text == "quit":
         send_message(sock, "QUT")
